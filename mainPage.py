cf70f9ed4c4aaf1724d2f399ddf3d0f4ee024837
winkawin2552 <winkawin2552@gmail.com>
2024-08-06 21:21:06 +0700

    make mainPage
    done make main page so now have to make sub page.

---
+++ mainPage.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import subPage
+
+root = Tk()
+root.geometry("499x281")
+root.minsize(499, 281)
+root.maxsize(499, 281)
+
+def lenghtMain(event):
+    root.destroy()
+    subPage.lenght("")
+def MetricMain(event):
+    root.destroy()
+    subPage.metric("")
+def SpeedMain(event):
+    root.destroy()
+    subPage.speed("")
+def TimeMain(event):
+    root.destroy()
+    subPage.time("")
+def VolumeMain(event):
+    root.destroy()
+    subPage.volume("")
+
+
+
+def resize(img_path, size):
+    image = Image.open(img_path)
+    resize_image = image.resize(size)
+    img = ImageTk.PhotoImage(resize_image)
+    return img
+
+
+metricPic = resize("picture/metric.png", (200, 200))
+lenghtPic = resize("picture/lenght.png", (200, 200))
+speedPic = resize("picture/speed.png", (200, 200))
+timePic = resize("picture/time.png", (200, 200))
+volumePic = resize("picture/volume.png", (200, 200))
+modePic = resize("picture/mode.png", (50, 50))
+
+canvas = Canvas(root, width=499, height=281, highlightthickness=0)
+canvas.grid(row=0, column=0, rowspan=5, columnspan=5)
+
+
+backgroundIMG = PhotoImage(file="picture/background.png")
+canvas.create_image(0, 0, anchor=NW, image=backgroundIMG)
+
+
+lenght = canvas.create_image(10, 0, anchor=NW, image=lenghtPic, tags="lenghtPic")
+metric= canvas.create_image(160, 0, anchor=NW, image=metricPic, tags="metricPic")
+speed = canvas.create_image(300, 0, anchor=NW, image=speedPic, tags="speedPic")
+time = canvas.create_image(10, 120, anchor=NW, image=timePic, tags="timePic")
+volume = canvas.create_image(160, 120, anchor=NW, image=volumePic, tags="volumePic")
+mode = canvas.create_image(5, 5, anchor=NW, image=modePic, tags="modePic")
+
+
+canvas.tag_bind("lenghtPic", "<Button-1>", lenghtMain)
+canvas.tag_bind("metricPic", "<Button-1>", MetricMain)
+canvas.tag_bind("speedPic", "<Button-1>", SpeedMain)
+canvas.tag_bind("timePic", "<Button-1>", TimeMain)
+canvas.tag_bind("volumePic", "<Button-1>", VolumeMain)
+
+root.mainloop()
---
+++ subPage.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import Tk
+
+def lenght(event):
+    root = Tk()
+    root.title("Lenght")
+
+    root.mainloop()
+
+def metric(event):
+    root = Tk()
+    root.title("Metric")
+
+    root.mainloop()
+
+def speed(event):
+    root = Tk()
+    root.title("Speed")
+
+    root.mainloop()
+
+def time(event):
+    root = Tk()
+    root.title("Time")
+
+    root.mainloop()
+
+def volume(event):
+    root = Tk()
+    root.title("Volume")
+
+    root.mainloop()
